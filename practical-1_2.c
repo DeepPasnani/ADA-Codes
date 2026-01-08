fc4b4ffa3cdec13a1c200c6c649a5a1a3eaca375
DeepPasnani <168401187+DeepPasnani@users.noreply.github.com>
2026-01-08 10:26:49 +0000

    Selection sort.

---
+++ practical-1_2.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void selectionsort(int arr[], int n) 
+{
+    int is_swapped = 0, i, j, min, temp;
+    for (i = 0; i < n-1; i++) 
+    {
+        min = i;
+        for (j = i+1; j < n; j++) 
+        {
+            if (arr[j] < arr[min]) 
+            {
+                min = j;
+                is_swapped = 1;
+            }
+        }
+        temp = arr[min];
+        arr[min] = arr[i];
+        arr[i] = temp;
+    }
+    if(is_swapped == 0)
+    {
+        printf("Elements are sorted.");
+        break;
+    }
+}
+
+void printArray(int arr[], int n) 
+{
+    printf("\nSorted Array: ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+} 
+
+int main()
+{
+    int xarr[50], n, m;
+    printf("Enter the number of values in the array: ");
+    scanf("%d", &m);
+    printf("Enter the values in the array: \n");
+    for(int i = 0; i < m; i++)
+    {
+        printf("Value %d: ", i+1);
+        scanf("%d", &xarr[i]);
+    }
+    selectionsort(xarr, m);
+    printArray(xarr, m);
+    return 0;
+}
