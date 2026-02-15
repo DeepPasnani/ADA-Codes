fbc26cf4b71da43f57e3494d4c1080f9901e7b77
DeepPasnani <168401187+DeepPasnani@users.noreply.github.com>
2026-02-15 10:41:20 +0000

    Insertion Sort.

---
+++ practical-1.3.c
@@ -0,0 +1,32 @@
+#include <stdlib.h> 
+#include <stdio.h> 
+
+void insertionSort(int arr[], int n) 
+{ 
+    for(int i=1;i<n;++i) 
+    {    
+        int key=arr[i]; int j=i-1; 
+        while(j>=0 && arr[j]>key) 
+        { 
+            arr[j+1]=arr[j]; 
+            j=j-1; 
+        } 
+        arr[j+1]=key; 
+    } 
+} 
+
+void main() 
+{ 
+    { 
+        int i,arr[10],n=10; 
+        for(i=0;i<n;i++) 
+        { 
+            printf("\nEnter element %d to sort: ", i + 1); scanf("%d", &arr[i]); 
+        } 
+        insertionSort(arr, n); 
+        for(i=0;i<n;i++) 
+        { 
+            printf("\nSorted Array is : %d", arr[i]); 
+        } 
+    }
+}
